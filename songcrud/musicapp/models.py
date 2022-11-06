06c14db40a0d7aecf96b3619e3b7eb228b3d4246
Kim_Farida <farimomoh@gmail.com>
2022-11-06 20:33:36 +0000

    Refactor

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import imp
+
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name 
+
+class Song(models.Model):
+    artiste_rel = models.ForeignKey(Artiste, blank=True, null=True, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=150)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    song_rel = models.ForeignKey(Song, blank=True, null=True, on_delete=models.CASCADE)
+
+    content = models.CharField(max_length=500)
+    song_id = models.IntegerField()
+
+    def __str__(self) -> str:
+        if len(self.content) > 50:
+            return self.content
+        else:
+            return self.content[:50] + "..."
