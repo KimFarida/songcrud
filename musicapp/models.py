087ff89be888f0d691a50284c595fe77bb5b7023
KimFarida <95109808+KimFarida@users.noreply.github.com>
2022-11-06 18:22:18 +0100

    Code Refactor/Bug Fix

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,31 +1,30 @@
-import imp
-from datetime import datetime
-from email.policy import default
 from django.db import models
+import imp
+
 
 # Create your models here.
-
 class Artiste(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
-    def __str__(self) -> str:
-        return self.first_name, self.last_name
+    def __str__(self):
+        return self.first_name 
 
 class Song(models.Model):
-    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    artiste_rel = models.ForeignKey(Artiste, blank=True, null=True, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=150)
     date_released = models.DateField()
     likes = models.IntegerField()
     artiste_id = models.IntegerField()
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
 
 class Lyric(models.Model):
-    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song_rel = models.ForeignKey(Song, blank=True, null=True, on_delete=models.CASCADE)
+
     content = models.CharField(max_length=500)
     song_id = models.IntegerField()
 
@@ -34,4 +33,3 @@ class Lyric(models.Model):
             return self.content
         else:
             return self.content[:50] + "..."
-
